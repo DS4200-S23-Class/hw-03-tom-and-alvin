8c0270712669280b2b179559090a8339440a4d5f
Alvin Kannalath <akannalath@gmail.com>
2023-02-07 18:16:04 -0500

    Final Part done

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 // Function for when the button is clicked
 function buttonClicked() {
-	// If the background color is currently equal to black, change the background to white. If not, make it black
+	// If the background color is currently equal to black, change the background to white and
+  // the text to black. If not, make the background black and text white. It is essentially a
+  // light mode and dark mode.
 	if (document.body.style.backgroundColor === "black") {
     document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
   } else {
     document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
   }
 
 }
