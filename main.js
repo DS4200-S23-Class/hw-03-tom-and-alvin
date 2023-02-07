dc3f56c22acd49d852a4bb6f0e2edc71c8c383a4
Tom McDonagh <tommcdonagh@Toms-MacBook-Pro.local>
2023-02-07 18:02:09 -0500

    button added

---
+++ main.js
@@ -0,0 +1,10 @@
+// Function for when the button is clicked
+function buttonClicked() {
+	// If the background color is currently equal to black, change the background to white. If not, make it black
+	if (document.body.style.backgroundColor === "black") {
+    document.body.style.backgroundColor = "white";
+  } else {
+    document.body.style.backgroundColor = "black";
+  }
+
+}
